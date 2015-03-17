2b3477558b8250930002ce8e168df59d0d2cf275
Ted Shaffer <ted@brightsign.biz>
2015-03-16 17:44:19 -0700

    moving functions around

---
+++ jtr/webSite/js/common.js
@@ -0,0 +1,194 @@
+﻿function selectRecordedShows() {
+    switchToPage("recordedShowsPage");
+    getRecordedShows();
+}
+
+function switchToPage(newPage) {
+
+    // ???? - still necessary?
+    //if (clientType == "BrightSign") {
+    //    bsMessage.PostBSMessage({ message: "switch to " + newPage });
+    //}
+
+    var newPageId = "#" + newPage;
+    $(currentActiveElementId).css("display", "none");
+    currentActiveElementId = newPageId;
+    $(currentActiveElementId).removeAttr("style");
+    if (currentActiveElementId == "#homePage") {
+        $("#footerArea").empty();
+        $("#trickModeKeys").css("display", "block");
+
+        // ensure that the first element is highlighted and has focus
+        // TODO - make this more general purpose?
+        for (i = 0; i < mainMenuIds.length; i++) {
+            for (j = 0; j < mainMenuIds[i].length; j++) {
+                var elementId = "#" + mainMenuIds[i][j];
+
+                if (i != 0 || j != 0) {
+                    $(elementId).removeClass("btn-primary");
+                    $(elementId).addClass("btn-secondary");
+                }
+                else {
+                    $(elementId).removeClass("btn-secondary");
+                    $(elementId).addClass("btn-primary");
+                    $(elementId).focus();
+                }
+            }
+        }
+
+    } else {
+        $("#footerArea").append("<button class=\"btn btn-primary\" onclick=\"selectHomePage()\">Home</button><br><br>");
+        $("#ipAddress").css("display", "none");
+        $("#trickModeKeys").css("display", "none");
+    }
+}
+
+
+function getRecordedShows() {
+
+    console.log("getRecordedShows() invoked");
+
+    var aUrl = baseURL + "getRecordings";
+
+    $.ajax({
+        type: "GET",
+        url: aUrl,
+        dataType: "json",
+        success: function (recordings) {
+
+            // convert freespace from disk space to time (approximation) and display it
+            var freeSpace = recordings.freespace;
+            // 44934K per minute - sample 1 - long recording
+            // 43408K per minute - sample 2 - 11 minute recording
+            // use 44Mb per minute
+            var freeSpaceInMegabytes = Number(freeSpace);
+            var freeSpaceInMinutes = Math.floor(freeSpaceInMegabytes / 44);
+            var freeSpaceInHours = Math.floor(freeSpaceInMinutes / 60);
+
+            var freeSpace = "Free space: ";
+            if (freeSpaceInHours > 0) {
+                if (freeSpaceInHours > 1) {
+                    freeSpace += freeSpaceInHours.toString() + " hours";
+                }
+                else {
+                    freeSpace += "1 hour";
+                }
+                var partialFreeSpaceInMinutes = freeSpaceInMinutes % (freeSpaceInHours * 60);
+                if (partialFreeSpaceInMinutes > 0) {
+                    freeSpace += ", " + partialFreeSpaceInMinutes.toString() + " minutes";
+                }
+            }
+            else {
+                freeSpace += freeSpaceInMinutes.toString() + " minutes";
+            }
+            $("#recordedShowsRemainingSpace").text(freeSpace);
+
+            // display show recordings
+            jtrRecordings = recordings.recordings;
+
+            var toAppend = "";
+            var recordingIds = [];
+
+            $("#recordedShowsTableBody").empty();
+
+            _currentRecordings = {};
+
+            $.each(jtrRecordings, function (index, jtrRecording) {
+                toAppend += addRecordedShowsLine(jtrRecording);
+                recordingIds.push(jtrRecording.RecordingId);
+                _currentRecordings[jtrRecording.RecordingId] = jtrRecording;
+            });
+
+            // is there a reason to do this all at the end instead of once for each row?
+            $("#recordedShowsTableBody").append(toAppend);
+
+            recordedPageIds.length = 0;
+
+            // get last selected show from local storage - navigate to it. null if not defined
+            var url = baseURL + "lastSelectedShow";
+
+            $.get(url, {})
+                .done(function (result) {
+                    console.log("lastSelectedShow successfully sent");
+                    var lastSelectedShowId = result;
+
+                    var focusApplied = false;
+
+                    // add button handlers for each recording - note, the handlers need to be added after the html has been added!!
+                    $.each(recordingIds, function (index, recordingId) {
+
+                        // play a recording
+                        var btnIdRecording = "#recording" + recordingId;
+                        $(btnIdRecording).click({ recordingId: recordingId }, playSelectedShow);
+
+                        // delete a recording
+                        var btnIdDelete = "#delete" + recordingId;
+                        $(btnIdDelete).click({ recordingId: recordingId }, deleteSelectedShow);
+
+                        // highlight the last selected show
+                        if (recordingId == lastSelectedShowId) {
+                            focusApplied = true;
+                            $(btnIdRecording).focus();
+                        }
+
+                        var recordedPageRow = [];
+                        recordedPageRow.push(btnIdRecording);
+                        recordedPageRow.push(btnIdDelete);
+                        recordedPageIds.push(recordedPageRow);
+
+                    });
+
+                    if (!focusApplied) {
+                        $(recordedPageIds[0][0]).focus();
+                    }
+                })
+                .fail(function (jqXHR, textStatus, errorThrown) {
+                    debugger;
+                    console.log("lastSelectedShow failure");
+                })
+                .always(function () {
+                    //alert("lastSelectedShow finished");
+                });
+        }
+    });
+}
+
+
+function selectChannelGuide() {
+
+}
+
+
+function selectToDoList() {
+    switchToPage("toDoListPage");
+    getToDoList();
+}
+
+
+function getToDoList() {
+    var aUrl = baseURL + "toDoList";
+
+    $.ajax({
+        type: "GET",
+        url: aUrl
+    })
+    .done(function (result) {
+        var toAppend = "";
+
+        for (i = 0; i < result.length; i++) {
+            toAppend += "<tr id=\"toDoListRow" + i + " \"><td>date to record</td><td>title</td></tr>";
+        }
+        $("#recordedShowsTableBody").append(toAppend);
+    });
+}
+
+
+function selectHomePage() {
+    switchToPage("homePage");
+}
+
+
+function here(argument) {
+    console.log(argument);
+}
+
