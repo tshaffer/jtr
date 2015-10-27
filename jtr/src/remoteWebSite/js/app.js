7a37a6da8863684b518ad9bdb3da5032b754b3cd
tshaffer <shaffer.family@gmail.com>
2015-10-26 17:03:57 -0700

    Initial prototyping with models. I need help.

--- jtr/src/remoteWebSite/js/app.js
+++ jtr/src/remoteWebSite/js/app.js
@@ -4,23 +4,6 @@
 
 $(document).ready(function () {
 
-
-    ManualRecordView = Backbone.View.extend({
-
-        initialize: function () {
-            console.log("ManualRecordView::initialize");
-            this.template = _.template($('#manualRecordTemplate').html());
-            this.render();
-        },
-
-        render: function () {
-            console.log("ManualRecordView::render");
-            this.$el.html(this.template()); // this.$el is a jQuery wrapped el var
-            $("#manualRecordPage").css("display", "block");
-            return this;
-        }
-    }),
-
     MainMenuView = Backbone.View.extend({
 
         initialize: function () {
@@ -47,6 +30,44 @@ $(document).ready(function () {
         }
     });
 
+    ManualRecordModel = Backbone.Model.extend({
+        defaults: {
+            title: '',
+            duration: '',
+            date: 0,
+            time: 0,
+            inputSource: 0
+        }
+    });
+
+    ManualRecordView = Backbone.View.extend({
+
+        initialize: function () {
+            console.log("ManualRecordView::initialize");
+            this.template = _.template($('#manualRecordTemplate').html());
+            this.render();
+        },
+
+        render: function () {
+            console.log("ManualRecordView::render");
+            this.$el.html(this.template()); // this.$el is a jQuery wrapped el var
+            $("#manualRecordPage").css("display", "block");
+            return this;
+        },
+
+        events: {
+            "click #btnSetManualRecord": "executeManualRecordHandler"
+        },
+
+        executeManualRecordHandler: function( event ) {
+            console.log("executeManualRecordHandler, trigger executeManualRecord");
+            this.trigger("executeManualRecord");
+            return false;
+        }
+
+    });
+
+
 
     var mainMenuView = new MainMenuView({el: $("#homePage")});
 
@@ -61,7 +82,6 @@ $(document).ready(function () {
     //    var manualRecordView = new ManualRecordView({el: $("#manualRecordPage")});
     //
     //});
-
 
     //MainMenuController = ({
     //
@@ -84,19 +104,52 @@ $(document).ready(function () {
     //var mainMenuController = new MainMenuController();
     //mainMenuController.initialize();
 
+    // hack until Joel tells me how to do this or I figure it out
+    var manualRecordView;
+    var ManualRecordController;
+
     var MainMenuController = {
         initialize: function() {
             this.listenTo(mainMenuView, "invokeRecordedShows", function() {
                 console.log("MainMenuController:: invokeRecordedShowsHandler event received");
             });
             this.listenTo(mainMenuView, "invokeManualRecord", function() {
+
                 console.log("MainMenuController:: invokeManualRecord event received");
+
                 $(mainMenuView.el).hide();
-                var manualRecordView = new ManualRecordView({el: $("#manualRecordPage")});
+
+                ManualRecordController.initialize();
+                //var manualRecordView = new ManualRecordView({el: $("#manualRecordPage")});
+                manualRecordView = new ManualRecordView({el: $("#manualRecordPage")});
+                ManualRecordController.setHandlers();
             });
         }
     };
     _.extend(MainMenuController, Backbone.Events);
     MainMenuController.initialize();
+
+    //var ManualRecordController = {
+    ManualRecordController = {
+        initialize: function() {
+
+            var manualRecordModel = new ManualRecordModel({
+                title: 'Title',
+                duration: '69'
+            });
+
+            // initialize values (just use defaults in real life for some of the items
+            $('#manualRecordTitle').val(manualRecordModel.get('title'));
+            $('#manualRecordDuration').val(manualRecordModel.get('duration'));
+        },
+        setHandlers: function() {
+            this.listenTo(manualRecordView, "executeManualRecord", function() {
+                console.log("ManualRecordController:: executeManualRecord event received");
+                return false;
+            });
+        }
+    };
+    _.extend(ManualRecordController, Backbone.Events);
+    //ManualRecordController.initialize();
 
 });
